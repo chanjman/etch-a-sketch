02d453167ddae73686725b65de0b362cd12da139
chanjman <mariozugaj@gmail.com>
2016-12-11 23:40:30 +0100

    A whole lot of changes

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+opacity = $('#toggletwoinput').val();
+roundness = $('#togglethreeinput').val();
+
+
+$(function setColor() {
+    $('#togglefourinput').on('change', function() {
+        brushColor = $('#togglefourinput').val();
+        colorHeader = {'color': brushColor};
+        $('h1').css(colorHeader);
+    });
+});
+
 
