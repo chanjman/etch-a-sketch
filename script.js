ca6b3d0e2be6a231c7a8cd29e4ed789edbb4e23e
chanjman <mariozugaj@gmail.com>
2016-12-12 11:54:06 +0100

    Add function to add table to .main-etch

--- script.js
+++ script.js
@@ -1,17 +1,56 @@
-//Get value from slider and set heading back color
+//Get value from .toggleone
+//Get value from .togglethree
+
+
+
+//Get value from slider and set .heading back color
 
 $(function () {
-    $('#togglefourinput').on('change', function() {
-        var brushColor = $('#togglefourinput').val();
+    $('#togglethreeinput').on('change', function() {
+        var brushColor = $('#togglethreeinput').val();
         $('.heading').css({'background-color': brushColor})
     });
 });
 
-//Get value from slider and set heading roundness
+//Get value from slider and set .heading roundness
 
 $(function () {
-    $('#togglethreeinput').on('change', function () {
-       var brushRound = $('#togglethreeinput').val();
+    $('#toggletwoinput').on('change', function () {
+        var brushRound = $('#toggletwoinput').val();
         $('.heading').css({'border-radius': brushRound + 'px'})
-    })
+    });
+});
+
+//Get value from slider and set .tbldiv back color
+
+$(function () {
+    $('#togglethreeinput').on('change', function() {
+        var brushColor = $('#togglethreeinput').val();
+        $('.tbldiv').css({'background-color': brushColor})
+    });
+});
+
+//Remove table
+
+function removeTable () {
+    $('.tbldiv').remove();
+}
+
+function getDivSize () {
+    tbldiv_size = (100 / brushSize + '%');
+    return tbldiv_size;
+}
+
+//Get value from brush size slider and add table
+
+$(function addTable () {
+        $('#toggleoneinput').on('change', function () {
+            removeTable();
+            brushSize = $('#toggleoneinput').val();
+            for (var i = 1; i <= Math.pow(brushSize, 2); i++) {
+                $('<div class="tbldiv"></div>').appendTo('.main-etch');
+                getDivSize();
+                $('.tbldiv').css({'width': tbldiv_size, 'height': tbldiv_size})
+            }
+        });
 });
